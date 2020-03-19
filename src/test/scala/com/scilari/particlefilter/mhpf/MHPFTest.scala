18b40da6f650986f67604a3a9940f544f0d7c001
Ilari Vallivaara <ilari.vallivaara@gmail.com>
2020-03-19 11:28:25 +0200

    Fix tests. Add more cases.

--- src/test/scala/com/scilari/particlefilter/mhpf/MHPFTest.scala
+++ src/test/scala/com/scilari/particlefilter/mhpf/MHPFTest.scala
@@ -15,7 +15,6 @@ class MHPFTest extends FlatSpec{
       // situations
       val rmse = runForRMSE(pf, pr)
       rmse
-
     }
 
     val meanError = errors.sum/errors.size
@@ -25,33 +24,33 @@ class MHPFTest extends FlatSpec{
   }
 
   it should "give better results than fixed-likelihood PF with changing uncertainty " +
-    "when motion model uncertainty is small compared to measurement model" in {
+    "especially when motion model uncertainty is small compared to measurement model" in {
     val stepCount = 100
     val particleCount = 100
-    val devMotion = 0.5 //math.sqrt(10) // small value here is working
+    val devMotion = 0.5 // small value here
     val devMeasurementSmall = 2.0
     val devMeasurementLarge = 30.0
-    val conditionSmallDev = (x: Double) => math.abs(x % 10.0) < 5.0// ath.abs(x) < 10
+    val conditionSmallDev = (x: Double) => math.abs(x % 10.0) < 5.0
 
     val runCount = 100
 
     type Process = NonLinearRandomProcess
-    //type Process = NonLinearRandomProcessGrowing
+    // TODO: although usually better MHPF diverges on this case more often than fixed dev => investigate why
+    // type Process = NonLinearRandomProcessGrowing
 
     def errorFunction(pf: MinimalParticleFilter, pr: NonLinearRandomProcess) = runForRMSE(pf, pr)
 
     def prSmall = new Process(
-      stepCount = stepCount, devMeasurement = devMeasurementSmall
+      stepCount = stepCount, devMeasurement = devMeasurementSmall, devMotion = devMotion
     )
 
-
     def prLarge = new Process(
-      stepCount = stepCount, devMeasurement = devMeasurementLarge
+      stepCount = stepCount, devMeasurement = devMeasurementLarge, devMotion = devMotion
     )
 
     def prCombined = new Process(
       stepCount = stepCount, devMeasurement = devMeasurementSmall, devMeasurementLarge = devMeasurementLarge,
-      smallDevMeasurementCondition = conditionSmallDev
+      smallDevMeasurementCondition = conditionSmallDev, devMotion = devMotion
     )
 
     def errorsForDev(devs: Seq[Double], prToRunAgainst: => NonLinearRandomProcess): Seq[Double] = {
@@ -73,7 +72,7 @@ class MHPFTest extends FlatSpec{
     }
 
     val (optimalSmallDev, optimalSmallError) = {
-      val devs = 0.1 until devMeasurementSmall + 1.0 by 0.1
+      val devs = 0.5 until devMeasurementSmall + 2.0 by 0.1
       val errors = errorsForDev(devs, prSmall)
       (devs zip errors).minBy{_._2}
     }
@@ -85,7 +84,7 @@ class MHPFTest extends FlatSpec{
     }
 
     val (optimalCombinedDev, optimalCombinedError) = {
-      val devs = (0.1 until devMeasurementSmall + 1.0 by 0.1) ++ (devMeasurementSmall + 2.0 until devMeasurementLarge + 5 by 1.0)
+      val devs = (0.1 until devMeasurementSmall + 2.0 by 0.1) ++ (devMeasurementSmall + 2.0 until devMeasurementLarge + 5 by 1.0)
       val errors = errorsForDev(devs, prCombined)
       (devs zip errors).minBy{_._2}
     }
@@ -97,12 +96,12 @@ class MHPFTest extends FlatSpec{
     val errors: Seq[(Double, Double)] = for(run <- 0 until runCount) yield {
       val prActual = new Process(
         stepCount = stepCount, devMeasurement = devMeasurementSmall, devMeasurementLarge = devMeasurementLarge,
-        smallDevMeasurementCondition = conditionSmallDev
+        smallDevMeasurementCondition = conditionSmallDev, devMotion = devMotion
       )
 
       val prMhpf = new Process(
         stepCount = stepCount, devMeasurement = optimalSmallDev, devMeasurementLarge = optimalLargeDev,
-        smallDevMeasurementCondition = conditionSmallDev
+        smallDevMeasurementCondition = conditionSmallDev, devMotion = devMotion
       )
 
       val prFixed = new Process(
@@ -125,6 +124,59 @@ class MHPFTest extends FlatSpec{
 
     meanErrorMhpf should be < meanErrorFixed
 
+  }
+
+  it should "implicitly handle outliers assuming they are detected (often possible)" in {
+    val steps = 100
+    val particleCount = 100
+    val runCount = 100
+
+    // 20% of totally broken points (outliers)
+    val devLarge = 1000.0
+    val conditionSmall = (x: Double) => math.abs(x % 10.0) < 8.0
+
+    // Give 10% chance for false positives in outlier detection
+    val conditionSmallForMhpf = (x: Double) => conditionSmall(x) && math.random() < 0.9
+
+    val devMotion = 0.5
+
+    val errors: Seq[(Double, Double)] = for(run <- 0 until runCount) yield {
+
+      val prActual = new NonLinearRandomProcess(
+        stepCount = steps,
+        devMeasurementLarge = devLarge,
+        smallDevMeasurementCondition = conditionSmall,
+        devMotion = devMotion
+      )
+
+      val prMhpf = new NonLinearRandomProcess(
+        stepCount = steps,
+        devMeasurementLarge = devLarge,
+        smallDevMeasurementCondition = conditionSmallForMhpf,
+        devMotion = devMotion
+      )
+
+      val prFixed = new NonLinearRandomProcess(
+        stepCount = steps,
+        devMotion = devMotion
+      )
+
+      val mhpf = new MinimalParticleFilter(particleCount, prMhpf, resampleThreshold = 0.5)
+      val pfFixed = new MinimalParticleFilter(particleCount, prFixed, resampleThreshold = 0.5)
+
+      val errorMhpf = runForRMSE(mhpf, prActual)
+      val errorFixed = runForRMSE(pfFixed, prActual)
+
+      println("mhpf: " + errorMhpf + " fixed: " + errorFixed)
+      (errorMhpf, errorFixed)
+    }
+
+    val (errorsMhpf, errorsFixed) = errors.unzip
+    val meanErrorMhpf = errorsMhpf.sum/errorsMhpf.size
+    val meanErrorFixed = errorsFixed.sum/errorsFixed.size
+    println("Mean error mhpf: " + meanErrorMhpf + " mean error fixed: " + meanErrorFixed)
+
+    meanErrorMhpf should be < meanErrorFixed
   }
 
 }
